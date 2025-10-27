67fdba6c581b234d4e2e7d87697c4935baf28d7c
arbmarta <alexjfmartin@gmail.com>
2025-10-27 12:19:18 +0000

    Added per capita energy consumption

--- src/data_collector.py
+++ src/data_collector.py
@@ -39,7 +39,7 @@ print("Data saved to: ../data/API/global_energy_consumption.csv")
 
 # Fetch the metadata
 metadata = requests.get(
-    "https://ourworldindata.org/grapher/global-energy-substitution.metadata.json?v=1&csvType=full&useColumnShortNames=true"
+    "https://ourworldindata.org/grapher/primary-energy-cons.metadata.json?v=1&csvType=full&useColumnShortNames=true"
 ).json()
 
 # Save the metadata to JSON
