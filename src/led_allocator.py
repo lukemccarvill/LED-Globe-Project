838a997fef27bb9ee25901c2be2d8d884e349c2d
An Mei Daniels <amybda2004@outlook.com>
2025-10-28 15:32:35 +0000

    Changed method of allocating LEDs to use points grid

--- src/led_allocator.py
+++ src/led_allocator.py
@@ -1,12 +1,12 @@
 import os
-import numpy as np
+#import numpy as np
 import pandas as pd
-import rasterio
-from shapely.geometry import Point
+#import rasterio
+#from shapely.geometry import Point
 import geopandas as gpd
-from rasterio.mask import mask
+#from rasterio.mask import mask
 
-def allocate_leds(led_data, world, raster_path, allocate_leds=True, use_edited_geojson=False, manual_manipulation=False, geojson_output_path="led_positions_for_manual_edit.geojson", prev_edited_geojson_path="prev_edited_led_positions.geojson"):
+def allocate_leds(led_data, energy_timeseries, year, allocate_leds=True, place_ocean=True, use_edited_geojson=False, manual_manipulation=False, geojson_output_path="led_positions_for_manual_edit.geojson", prev_edited_geojson_path="prev_edited_led_positions.geojson"):
     if not allocate_leds:
         print("LED allocation skipped as draw_leds is set to False.")
         return gpd.GeoDataFrame()  # Return an empty GeoDataFrame
@@ -18,55 +18,65 @@ def allocate_leds(led_data, world, raster_path, allocate_leds=True, use_edited_g
 
     all_leds_gdf = gpd.GeoDataFrame()  # Stores all valid LED positions
 
-    with rasterio.open(raster_path) as src:
-        nodata_value = src.nodata  # Get the No Data value from the raster
+    for index,row in led_data.iterrows():
 
-        for index, row in led_data.iterrows():
-            country_name = row['Entity']
-            num_leds = int(row['Round'])
-            leds_placed = 0
+        country_name = row['Entity']
+        num_leds = int(row['Round'])
+        leds_placed = 0
 
-            try:
-                # Get the country's geometry from the world GeoDataFrame
-                country_geom = world[world['ADMIN'] == country_name].geometry.iloc[0]
-                out_image, out_transform = mask(src, [country_geom], crop=True)
-                population_array = out_image[0]
+        values_array = energy_timeseries[energy_timeseries['country'] == country_name]
+        values_array = values_array[["point_index", f"{year}", "geometry"]].sort_values(f"{year}", ascending=False)
 
-                flat_population = population_array.flatten()
-                sorted_indices = flat_population.argsort()[::-1]  # Sort by population density (highest first)
+        available_cells = len(values_array)
+        missing_leds = num_leds - available_cells
+        
+        if available_cells == 0:
+            print(f"Could not find {country_name} in raster data, skipping...")
 
-                available_cells = len(sorted_indices)
-                if num_leds > available_cells:
-                    print(f"Warning: {country_name} requested {num_leds} LEDs, but only {available_cells} cells are available. Attempting to place {available_cells} LEDs.")
+        else:
 
-                rows, cols = divmod(sorted_indices[:num_leds], population_array.shape[1])
+            for leds in range(0,num_leds-leds_placed): # Place LEDs on the land-space
 
-                for r, c in zip(rows, cols):
-                    pop_value = population_array[r, c]
-
-                    # Stop placing LEDs when encountering problematic values
-                    if pop_value <= 0 or pop_value == nodata_value:
-                        print(f"Encountered problematic value for {country_name}. Stopping LED placement. {leds_placed} LEDs placed out of {num_leds} requested.")
-                        break
-
-                    # Calculate the geographical coordinates of the LED position
-                    x, y = out_transform * (c + 0.5, r + 0.5)
-                    all_leds_gdf = pd.concat([all_leds_gdf, gpd.GeoDataFrame({
-                        'geometry': [Point(x, y)],
-                        'Country': [country_name],
-                        'Pop_Density': [pop_value]
-                    }, geometry='geometry')], ignore_index=True)
-
+                if leds_placed < available_cells: 
+                    
+                    all_leds_gdf = pd.concat([all_leds_gdf, gpd.GeoDataFrame({'geometry': [values_array["geometry"].iloc[leds]],
+                                                                            'Country': [country_name],
+                                                                            'Raster_Density': [values_array[f"{year}"].iloc[leds]]
+                                                                            }, geometry='geometry')], ignore_index=True)
                     leds_placed += 1
 
-                if leds_placed < num_leds:
-                    print(f"Warning: {country_name} requested {num_leds} LEDs, but only {leds_placed} were placed due to problematic values.")
+                else:
 
-            except IndexError:
-                print(f"Could not find geometry for {country_name}, skipping...")
+                    if place_ocean == True:
 
-    # Set the CRS of the GeoDataFrame
-    all_leds_gdf.set_crs(src.crs, inplace=True)
+                        if leds_placed >= num_leds:
+                            break
+
+                        print(f"Warning: {country_name} requested {num_leds} LEDs, but only {available_cells} cells are available. Attempting to place {missing_leds} LEDs in surrounding area.")
+                        values_sorted = energy_timeseries.iloc[energy_timeseries.geometry.x.argsort().values].reset_index(drop=True)
+                        surround = 1
+                        
+                        while leds_placed < num_leds:
+
+                            values_sorted_filtered = values_sorted[values_sorted['country'] == country_name]
+                            surround_indices = [x-surround for x in values_sorted_filtered.index] + [x+surround for x in values_sorted_filtered.index] 
+                            surround_indices = values_sorted.loc[surround_indices].query("country.isnull()").index
+                            values_sorted.loc[surround_indices, "country"] = country_name
+
+                            for leds in surround_indices: # Place remaining LEDs on the land-space
+                                all_leds_gdf = pd.concat([all_leds_gdf, gpd.GeoDataFrame({'geometry': [values_sorted["geometry"].iloc[leds]],
+                                                                                        'Country': [country_name],
+                                                                                        'Raster_Density': [values_sorted[f"{year}"].iloc[leds]]
+                                                                                        }, geometry='geometry')], ignore_index=True)
+                                leds_placed += 1
+                                if leds_placed >= num_leds:
+                                    break
+
+                            surround += 1
+                    
+                    else: 
+                        print(f"Warning: {country_name} requested {num_leds} LEDs, but only {leds_placed} were placed due to not having enough space.")
+                        break
 
     # If manual manipulation is enabled, output the GeoJSON to the transients folder
     if manual_manipulation:
